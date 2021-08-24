2d444df408e9e2fa7d8be70b80a3d952ed90b542
Pablo Neres <pabloneres@hotmail.com>
2021-08-24 00:28:31 -0300

    jwt

--- start/routes.js
+++ start/routes.js
@@ -106,22 +106,22 @@ Route.post('/mqtt', 'MqttController.store')
 Route.group(() => {
   Route.get('/', 'EquipamentoController.index')
   Route.post('/', 'EquipamentoController.store')
-}).prefix('equipamento')
+}).prefix('equipamento').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'RoleController.index')
   Route.post('/', 'RoleController.store')
-}).prefix('roles')
+}).prefix('roles').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'DepartmentController.index')
   Route.post('/', 'DepartmentController.store')
-}).prefix('department')
+}).prefix('department').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'DepartmentRoleController.index')
   Route.post('/', 'DepartmentRoleController.store')
-}).prefix('departmentRoles')
+}).prefix('departmentRoles').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'LaboratorioController.index')
@@ -129,14 +129,14 @@ Route.group(() => {
   Route.post('/:id', 'LaboratorioController.show')
   Route.put('/', 'LaboratorioController.update')
   Route.delete('/:id', 'LaboratorioController.destroy')
-}).prefix('laboratorio')
+}).prefix('laboratorio').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'LaboratorioServicoController.index')
   Route.post('/', 'LaboratorioServicoController.store')
   Route.put('/', 'LaboratorioServicoController.update')
   Route.delete('/:id', 'LaboratorioServicoController.destroy')
-}).prefix('laboratorioServicos')
+}).prefix('laboratorioServicos').middleware(['auth'])
 
 
 Route.group(() => {
@@ -144,7 +144,7 @@ Route.group(() => {
   Route.post('/:id', 'DepartmentClincController.store')
   Route.put('/', 'DepartmentClincController.update')
   Route.delete('/:id', 'DepartmentClincController.destroy')
-}).prefix('departmento_desconto')
+}).prefix('departmento_desconto').middleware(['auth'])
 
 
 Route.group(() => {
@@ -152,30 +152,30 @@ Route.group(() => {
   Route.post('/:id', 'FormaPagamentoController.store')
   Route.put('/:id', 'FormaPagamentoController.update')
   Route.delete('/:id', 'FormaPagamentoController.destroy')
-}).prefix('forma_pagamento')
+}).prefix('forma_pagamento').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'PagamentoOrcamentoController.index')
   Route.post('/', 'PagamentoOrcamentoController.store')
   Route.put('/:id', 'PagamentoOrcamentoController.update')
   Route.delete('/:id', 'PagamentoOrcamentoController.destroy')
-}).prefix('pagamento')
+}).prefix('pagamento').middleware(['auth'])
 
-Route.post('/pagamento_total', 'PagamentoOrcamentoController.total')
-Route.post('/pagamento_boleto', 'PagamentoOrcamentoController.boleto')
-Route.post('/negociacao', 'PagamentoOrcamentoController.negociacao')
+Route.post('/pagamento_total', 'PagamentoOrcamentoController.total').middleware(['auth'])
+Route.post('/pagamento_boleto', 'PagamentoOrcamentoController.boleto').middleware(['auth'])
+Route.post('/negociacao', 'PagamentoOrcamentoController.negociacao').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'MetodosPagamentoController.index')
   Route.post('/', 'MetodosPagamentoController.store')
-}).prefix('metodosPagamento')
+}).prefix('metodosPagamento').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'ProcedimentoExecucaoController.index')
   Route.post('/', 'ProcedimentoExecucaoController.store')
   Route.put('/:id', 'ProcedimentoExecucaoController.update')
   Route.delete('/:id', 'ProcedimentoExecucaoController.destroy')
-}).prefix('procedimentoExecucao')
+}).prefix('procedimentoExecucao').middleware(['auth'])
 
 
 Route.group(() => {
@@ -186,44 +186,44 @@ Route.group(() => {
 Route.group(() => {
   Route.get('/:id', 'TesteController.index')
   Route.post('/', 'TesteController.createBoleto')
-}).prefix('teste')
+}).prefix('teste').middleware(['auth'])
 
 
 Route.group(() => {
   Route.post('/', 'AssaController.event')
   Route.post('/clean', 'AssaController.clean')
   Route.post('/paymentCash/:id', 'AssaController.paymentCash')
-}).prefix('assas')
+}).prefix('assas').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'EspecialidadeDentistaController.index')
   Route.post('/:id', 'EspecialidadeDentistaController.store')
-}).prefix('especialidade_config')
+}).prefix('especialidade_config').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'ComissaoConfigController.index')
   Route.post('/:id', 'ComissaoConfigController.store')
   Route.get('/:id', 'ComissaoConfigController.show')
-}).prefix('comissao_config')
+}).prefix('comissao_config').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'ComissaoController.index')
   Route.get('/:id', 'ComissaoController.show')
   Route.put('/:id', 'ComissaoController.update')
-}).prefix('comissoes')
+}).prefix('comissoes').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'FichaClinicaController.index')
   Route.get('/:id', 'FichaClinicaController.show')
   Route.post('/updateTitular', 'FichaClinicaController.updateTitular')
-}).prefix('fichaClinica')
+}).prefix('fichaClinica').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/', 'NegociacaoController.index')
   Route.get('/:id', 'NegociacaoController.show')
   Route.put('/:id', 'NegociacaoController.update')
-}).prefix('faturamento')
+}).prefix('faturamento').middleware(['auth'])
 
 Route.group(() => {
   Route.get('/:id', 'NegociacaoBoletoController.update')
-}).prefix('negociacao_boleto')
+}).prefix('negociacao_boleto').middleware(['auth'])
